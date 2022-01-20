c4dcef5519abb8f5a47be4314c2682c7f5161562
ufukcanli <iletisim@ufukcanli.com>
2022-01-20 22:17:39 +0300

    Add initial setup

---
+++ Homework2/Homework2/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  Homework2
+//
+//  Created by Ufuk Canlı on 20.01.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .systemGreen
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+}
+
